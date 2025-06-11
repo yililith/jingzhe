77cdad9789125d7ae6d7ecc84acfda7e7084b808
kaichun <lovekaichun@outlook.com>
2025-06-11 19:42:11 +0800

    面包屑完成

--- src/layout/components/layout_breadcrumb.vue
+++ src/layout/components/layout_breadcrumb.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import type { BreadcrumModel } from '@/model/model_menu.ts'
 const route = useRoute()
 
 const router = useRouter()
 
-const breadcrumbList = ref<BreadcrumModel[]>([{path: '/dashboard', label: '主页'}])
+const breadcrumbList = ref<BreadcrumModel[]>([])
 
 const routeJump = (r: string) =>{  
   router.push(r)
 }
-// 监控路由变化
+/**
+ * 监控路由变化，生成面包屑标签
+ */
 watch(
   () => route.path,
   () => {
-    route.matched.forEach((item) => {
+    route.matched.filter(item => item.children.length === 0).filter(item => item.meta.title).filter(item => !breadcrumbList.value.find(breadcrumb => breadcrumb.label === item.meta.title)).forEach((item) => {
       breadcrumbList.value.push({
         path: item.path,
         label: item.meta.title as string
@@ -24,13 +26,26 @@ watch(
   },
   { immediate: true },
 )
+
+/**
+ * 初始化面包屑标签
+ */
+onMounted(() => {
+  breadcrumbList.value = [{path: '/dashboard', label: '主页'}]
+})
+/**
+ * 提出被删除的breadcrumb
+ * @param bread string
+ */
+const removeBreadcrumb = (breadcrumb: string) => {
+  breadcrumbList.value = breadcrumbList.value.filter(item => item.label !== breadcrumb)
+}
 </script>
 
 <template>
   <a-space align="baseline" size="mini">
     <template v-for="({ path, label }, index) of breadcrumbList" :key="index">
-      <a-tag v-if="index === 0" color="rgb(255, 144, 71)" checkable :checked="true" @check="routeJump(path)">{{ label }}</a-tag>
-      <a-tag v-else color="rgb(255, 144, 71)" closable checkable :checked="true" @check="routeJump(path)">{{ label }}</a-tag>
+      <a-tag color="rgb(255, 144, 71)" :closable="index !== 0" checkable :checked="true" @check="routeJump(path)" @close="removeBreadcrumb(label)">{{ label }}</a-tag>
     </template>
   </a-space>
 </template>
