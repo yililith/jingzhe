425ace2f01fcf4eac7528ac423e29ddf20a7a753
kaichun <lovekaichun@outlook.com>
2025-06-12 18:41:48 +0800

    展开菜单问题

--- src/components/jz-card/index.vue
+++ src/components/jz-card/index.vue
@@ -13,6 +13,9 @@
   height: calc(100% - 10px);
   width: calc(100% - 6px);
   border-radius: 10px;
+  overflow-y: scroll;         /* 允许垂直滚动 */
+  scrollbar-width: none;    /* Firefox 隐藏滚动条 */
+  -ms-overflow-style: none; /* IE 10+ 隐藏滚动条 */
 }
 
 .comp-content::-webkit-scrollbar {
--- src/layout/components/layout_sider.vue
+++ src/layout/components/layout_sider.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import Logo from '@/assets/logo.svg'
+import type { MenuModel } from '@/model/model_menu'
 import { storeMenu } from '@/stores/store_menu'
 import { setMenus } from '@/utils/utils_menus'
-import { useRouter } from 'vue-router'
+import { log } from 'echarts/types/src/util/log.js'
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
 
 const router = useRouter()
 // 菜单生成
@@ -10,10 +15,33 @@ const menuStore = storeMenu()
 //
 const menuNameList = setMenus(menuStore.menuList)
 
-// 路由跳转
-const routeJump = (route: string) =>{  
-  router.push(route)
+const selectKeys = ref<string[]>([])
+
+const openKeys = ref<string[]>([])
+// 路由跳转 and 添加被选中的菜单key
+const routeJump = (item: MenuModel) =>{  
+  selectKeys.value = [item.name]
+  router.push(item.path)
 }
+
+/**
+ * 监听路由变化,并添加展开菜单的key
+ */
+watch(
+  () => route.path,
+  () => {
+    selectKeys.value = [route.meta.title as string]
+    
+    if (route.meta.grade === 'one') {
+      openKeys.value= []
+    }
+    
+    if (route.meta.grade === 'two') {
+      openKeys.value= [route.matched[1].meta.title as string]
+    }
+  },
+  { immediate: true },
+)
 
 </script>
 
@@ -25,16 +53,16 @@ const routeJump = (route: string) =>{
       </a-avatar>
     </div>
     <!--  菜单组件  -->
-    <a-menu :accordion="true">
+    <a-menu :accordion="true" v-model:selected-keys="selectKeys" v-model:open-keys="openKeys" :default-selected-keys="['主页']">
       <template v-for="item in menuNameList" :key="item.name">
-        <a-sub-menu v-if="item.children">
+        <a-sub-menu v-if="item.children" :key="item.name">
           <template #icon><component :is="item.icon"></component></template>
           <template #title>{{ item.name }}</template>
-          <a-menu-item v-for="child in item.children" :key="child.name" @click="routeJump(child.path)">
+          <a-menu-item v-for="child in item.children" :key="child.name" @click="routeJump(child)">
             {{ child.name }}
           </a-menu-item>
         </a-sub-menu>
-        <a-menu-item v-else @click="routeJump(item.path)">
+        <a-menu-item v-else @click="routeJump(item)" :key="item.name!">
           <template #icon><component :is="item.icon"></component></template>
           {{ item.name }}
         </a-menu-item>
